93a33b6e3e0f48c45fdcacda11f88898eedc300f
Douglas Freed <dwfreed@mtu.edu>
2011-11-28 14:05:57 -0500

    Fix which file the config parser opens

--- conf.c
+++ conf.c
@@ -9,7 +9,7 @@
 int load_config(GHashTable *config_table){
 	config_t config;
 	config_init(&config);
-	if( !config_read_file(&config, "wolfbot.conf") ){
+	if( !config_read_file(&config, "twitterbot.conf") ){
 		fprintf(stderr, "%s:%d: error: %s\n", config_error_file(&config), config_error_line(&config), config_error_text(&config));
 		config_destroy(&config);
 		return 0;
