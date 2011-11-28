6e6bb45ab27776fc0eb952e8614422957a70753a
Doug Freed <931883+dwfreed@users.noreply.github.com>
2011-11-28 14:04:33 -0500

    Add the callbacks for IRC events; still have to write them

---
+++ callbacks.c
@@ -0,0 +1,9 @@
+#include "global.h"
+
+void irc_event(struct irc_session *session, const char *event, const char *origin, const char **params, unsigned int count){
+	
+}
+
+void irc_numeric(struct irc_session *session, unsigned int event, const char *origin, const char **params, unsigned int count){
+	
+}
