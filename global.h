8b0b2c9b10f649e224854bcd449cd2959f0f1933
Doug Freed <931883+dwfreed@users.noreply.github.com>
2011-11-30 22:02:42 -0500

    Created a context struct to pass around like gonoherpasyphilaids, will probably add more to it later

--- global.h
+++ global.h
@@ -17,3 +17,22 @@ struct config_entry {
 	int size;
 	void *data;
 };
+
+struct context {
+	struct {
+		unsigned int run : 1;
+		unsigned int restart : 1;
+		unsigned int follow_config_changed : 1;
+		unsigned int pong_received : 1;
+	} flags;
+	GStaticRWLock *flags_lock;
+	GCond *pong_cond;
+	GMutex *pong_mutex;
+	GHashTable *config;
+	GStaticRWLock *config_lock;
+	GHashTable *nicks;
+	GAsyncQueue *raw_tweets;
+	GAsyncQueue *raw_messages;
+	GHashTable *channel_pipes_read;
+	GHashTable *channel_pipes_write;
+};
