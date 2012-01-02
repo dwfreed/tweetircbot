79b0bf4dfdf26aebd70f923609165adb80ce6e0b
Douglas Freed <dwfreed@mtu.edu>
2012-01-02 15:53:53 -0500

    Added callback code to push IRC messages to the message handler thread

--- callbacks.c
+++ callbacks.c
@@ -1,9 +1,27 @@
 #include "global.h"
 
 void irc_event(struct irc_session *session, const char *event, const char *origin, const char **params, unsigned int count){
-	
+	struct context *context = (struct context *)irc_get_ctx(session);
+	struct message *message = calloc(1, sizeof(struct message));
+	message->origin = strdup(origin);
+	message->event = strdup(event);
+	message->params = calloc(count, sizeof(char *));
+	unsigned int i;
+	for( i = 0; i < count; ++i ){
+		message->params[i] = strdup(params[i]);
+	}
+	g_async_queue_push(context->raw_messages, message);
 }
 
 void irc_numeric(struct irc_session *session, unsigned int event, const char *origin, const char **params, unsigned int count){
-	
+	struct context *context = (struct context *)irc_get_ctx(session);
+	struct message *message = calloc(1, sizeof(struct message));
+	message->origin = strdup(origin);
+	message->eventcode = event;
+	message->params = calloc(count, sizeof(char *));
+	unsigned int i;
+	for( i = 0; i < count; ++i ){
+		message->params[i] = strdup(params[i]);
+	}
+	g_async_queue_push(context->raw_messages, message);
 }
