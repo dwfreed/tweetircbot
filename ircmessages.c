dd5b69bda928b0f6daaba4dd64d5bd9541b4deb5
Doug Freed <931883+dwfreed@users.noreply.github.com>
2012-01-09 08:35:30 +0000

    Handle more IRC messages, start processing some commands, and add some auth checkers

--- ircmessages.c
+++ ircmessages.c
@@ -1,5 +1,74 @@
 #include "global.h"
 
+char isadmin(struct irc_session *session, char *origin){
+	struct context *context = irc_get_ctx(session);
+	char *nick = irc_target_get_nick(origin);
+	char *account = g_hash_table_lookup(context->nicks, nick);
+	free(nick);
+	if( account ){
+		int admins_size, i = 0;
+		char found = FALSE;
+		g_static_rw_lock_reader_lock(context->config_lock);
+		GList *admins = config_get_array(context->config, "bot.admins", &admins_size);
+		while( i < admins_size ){
+			char *admin = config_array_get_string(admins, i++);
+			if( !strcasecmp(admin, account) ){
+				found = TRUE;
+				break;
+			}
+		}
+		g_static_rw_lock_reader_unlock(context->config_lock);
+		return found;
+	} else {
+		return 0;
+	}
+}
+
+char ischanop(struct irc_session *session, char *channel, char *origin){
+	struct context *context = irc_get_ctx(session);
+	char *nick = irc_target_get_nick(origin);
+	char *account = g_hash_table_lookup(context->nicks, nick);
+	free(nick);
+	if( account ){
+		int chanops_size, i = 0;
+		char found = FALSE;
+		char *chanop_setting_path = g_strdup_printf("channels.%s.ops", channel);
+		g_static_rw_lock_reader_lock(context->config_lock);
+		GList *chanops = config_get_array(context->config, chanop_setting_path, &chanops_size);
+		while( i < chanops_size ){
+			char *chanop = config_array_get_string(chanops, i++);
+			if( !strcasecmp(chanop, account) ){
+				found = TRUE;
+				break;
+			}
+		}
+		g_static_rw_lock_reader_unlock(context->config_lock);
+		g_free(chanop_setting_path);
+		return found;
+	} else {
+		return FALSE;
+	}
+}
+
+void commands(struct irc_session *session, struct message *message, char *command_parts[]){
+	struct context *context = irc_get_ctx(session);
+	char *nick = irc_target_get_nick(message->origin);
+	char authorized = FALSE;
+	if( !strcasecmp(command_parts[0], "quit") ){
+		if( isadmin(session, message->origin) ){
+			g_static_rw_lock_writer_lock(context->flags_lock);
+			context->flags.run = 0;
+			g_static_rw_lock_writer_unlock(context->flags_lock);
+			irc_cmd_quit(session, "Bye!");
+			authorized = TRUE;
+		}
+	}
+	if( !authorized ){
+		irc_cmd_notice(session, nick, "You are not authorized to perform this command.");
+	}
+	free(nick);
+}
+
 void *ircmessages(void *args){
 	struct irc_session *session = (struct irc_session *)args;
 	struct context *context = irc_get_ctx(session);
@@ -22,22 +91,69 @@ void *ircmessages(void *args){
 						break;
 				}
 			} else {
-				if( !strcmp(message->event, "JOIN") ){
-					if( strcmp(message->params[1], "*") ){
-						g_hash_table_insert(context->nicks, irc_target_get_nick(message->origin), strdup(message->params[1]));
-					} else {
-						char *nick = irc_target_get_nick(message->origin);
-						g_hash_table_remove(context->nicks, nick);
+				if( !strcasecmp(message->event, "JOIN") ){
+					char *nick = irc_target_get_nick(message->origin);
+					if( !strcmp(nick, irc_get_nick(session)) ){
+						irc_send_raw(session, "WHO %s %%na", message->params[0]);
 						free(nick);
+					} else {
+						if( strcmp(message->params[1], "*") ){
+							g_hash_table_insert(context->nicks, nick, strdup(message->params[1]));
+						} else {
+							g_hash_table_remove(context->nicks, nick);
+							free(nick);
+						}
 					}
-				} else if( !strcmp(message->event, "ACCOUNT") ){
+				} else if( !strcasecmp(message->event, "ACCOUNT") ){
+					char *nick = irc_target_get_nick(message->origin);
 					if( strcmp(message->params[0], "*") ){
-						g_hash_table_insert(context->nicks, irc_target_get_nick(message->origin), strdup(message->params[0]));
+						g_hash_table_insert(context->nicks, nick, strdup(message->params[0]));
 					} else {
-						char *nick = irc_target_get_nick(message->origin);
 						g_hash_table_remove(context->nicks, nick);
 						free(nick);
 					}
+				} else if( !strcasecmp(message->event, "PRIVMSG") ){
+					char *nick = irc_get_nick(session);
+					size_t nick_length = strlen(nick);
+					char **text_parts = g_strsplit(message->params[1], " ", 0);
+					if( !strcmp(message->params[0], nick) ){
+						commands(session, message, text_parts);
+					} else if( !( strncmp(text_parts[0], nick, nick_length) || strncmp(text_parts[0] + nick_length, ":", 1) ) ){
+						commands(session, message, text_parts + 1);
+					}
+					g_strfreev(text_parts);
+				} else if( !strcasecmp(message->event, "CONNECT") ){
+					irc_send_raw(session, "CAP REQ :account-notify extended-join");
+					int channels_size;
+					g_static_rw_lock_reader_lock(context->config_lock);
+					GList *channels = config_get_array(context->config, "bot.channels", &channels_size);
+					char join_command[511] = "JOIN ";
+					int i = 0;
+					while( i < channels_size ){
+						while( strlen(join_command) < 505 ){
+							char *channel = config_array_get_string(channels, i);
+							if( strlen(join_command) + strlen(channel) + 1 > 505 ){
+								break;
+							} else {
+								if( strcmp(join_command, "JOIN ") ){
+									strcat(join_command, ",");
+								}
+								strcat(join_command, channel);
+								++i;
+							}
+							if( i >= channels_size ){
+								break;
+							}
+						}
+						irc_send_raw(session, join_command, NULL);
+						strcpy(join_command, "JOIN ");
+					}
+					g_static_rw_lock_reader_unlock(context->config_lock);
+				} else if( !strcasecmp(message->event, "ERROR") ){
+					irc_disconnect(session);
+					g_static_rw_lock_writer_lock(context->flags_lock);
+					context->flags.run = 0;
+					g_static_rw_lock_writer_unlock(context->flags_lock);
 				}
 			}
 			message_free(message);
