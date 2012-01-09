85073b5874a4126ac8448f20e262a6e2b586c04c
Doug Freed <931883+dwfreed@users.noreply.github.com>
2012-01-09 07:20:07 +0000

    Fix for when there is no origin

--- callbacks.c
+++ callbacks.c
@@ -3,7 +3,7 @@
 void irc_event(struct irc_session *session, const char *event, const char *origin, const char **params, unsigned int count){
 	struct context *context = (struct context *)irc_get_ctx(session);
 	struct message *message = calloc(1, sizeof(struct message));
-	message->origin = strdup(origin);
+	message->origin = origin ? strdup(origin) : NULL;
 	message->event = strdup(event);
 	message->params = calloc(count, sizeof(char *));
 	unsigned int i;
@@ -16,7 +16,7 @@ void irc_event(struct irc_session *session, const char *event, const char *origi
 void irc_numeric(struct irc_session *session, unsigned int event, const char *origin, const char **params, unsigned int count){
 	struct context *context = (struct context *)irc_get_ctx(session);
 	struct message *message = calloc(1, sizeof(struct message));
-	message->origin = strdup(origin);
+	message->origin = origin ? strdup(origin) : NULL;
 	message->eventcode = event;
 	message->params = calloc(count, sizeof(char *));
 	unsigned int i;
