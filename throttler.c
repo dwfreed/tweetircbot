c052fd351a04435e01b04fa13c7e4be6df78c86c
Douglas Freed <dwfreed@mtu.edu>
2012-01-17 01:06:59 +0000

    Finish the throttler thread function

--- throttler.c
+++ throttler.c
@@ -11,7 +11,31 @@ void *throttler(void *args){
 		g_time_val_add(&wait_time, 1000000);
 		g_mutex_lock(context->pong_mutex);
 		if( context->flags.pong_received ){
-			
+			char *message;
+			g_get_current_time(&wait_time);
+			g_time_val_add(&wait_time, 1000000);
+			if( ( message = g_async_queue_timed_pop(context->outgoing_messages, &wait_time) ) ){
+				irc_send_raw(session, message, NULL);
+				free(message);
+				int i;
+				for( i = 0; i < 3; ++i ){
+					if( ( message = g_async_queue_try_pop(context->outgoing_messages) ) ){
+						irc_send_raw(session, message, NULL);
+						free(message);
+					} else {
+						break;
+					}
+				}
+				context->flags.pong_received = FALSE;
+				irc_send_raw(session, "PING %s", irc_get_nick(session));
+				g_get_current_time(&wait_time);
+				g_time_val_add(&wait_time, 1000000);
+				if( g_cond_timed_wait(context->pong_cond, context->pong_mutex, &wait_time) ){
+					g_mutex_unlock(context->pong_mutex);
+				}
+			} else {
+				g_mutex_unlock(context->pong_mutex);
+			}
 		} else if( g_cond_timed_wait(context->pong_cond, context->pong_mutex, &wait_time) ){
 			g_mutex_unlock(context->pong_mutex);
 		}
